09949c8dfeaf97cd5346e85813353bd3eaba80be
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-10-18 09:10:36 -0400

    portals added
    script

---
+++ project1-7_classes/Portal.js
@@ -0,0 +1,15 @@
+class Portal extends GameObject{
+	constructor(message,image,x,y){
+		super(stairs,x,y);
+	//	this.sceneToGo = sceneToGo;
+		}
+
+		drawText(){
+			textSize(20);
+			fill('white');
+			text(this.message,width/2,height/2);
+
+			text("you are colliding",this.x-50,this.y-100);
+		}
+
+	}
---
+++ project1-7_classes/sketch.js
@@ -0,0 +1,103 @@
+/*
+
+Project 1-7 Classes 
+
+*/
+
+
+var blueIdle;
+var blueWalk;
+var blueWalkBack; 
+var blueWalkRight;
+var blueWalkLeft;
+var wallImage;
+var floor;
+var brickWall = [];
+var player;
+var jump;
+var stairs 
+var portals; 
+
+
+function preload (){
+	blueIdle = loadImage ("blueidle.gif");
+	blueWalk = loadImage ("bluewalk.gif");
+	blueWalkBack = loadImage ("bluewalkback.gif");
+	blueWalkRight = loadImage("bluewalkright.gif");
+	blueWalkLeft = loadImage ("bluewalkleft.gif");
+	wallImage = loadImage("brickwalltexture.png");
+	floor = loadImage("floor232.png");
+	jump =loadImage("jump.gif")
+	stairs = loadImage("stairs.png");
+}	
+
+function setup(){
+	createCanvas (400,400);
+	imageMode(CENTER);
+
+
+
+
+//	brickWall = new GameObject(wallImage,100,200);
+
+//	brickWall.push(new GameObject(wallImage,100,200));
+
+
+	for(let i =0;i < 13; i++ ){
+		for(let y =0; y < 200; y+= 32){
+		brickWall.push(new GameObject(wallImage,i*32,y));
+		}
+	}
+
+/*
+
+	for(let i =0;i < 13; i++ ){
+		let brick = new GameObject(wallImage,i*32,height-200);
+		brickWall.push(brick);
+	}
+
+*/``
+	player = new Player(300,250);
+
+	//for loop & .push are good for drawing multiple objects 
+	//portals.push(new Portal(stairs,300,300)) ;
+	portals = new Portal("you are colliding",stairs,300,300);
+}	
+
+
+
+function draw(){
+	
+
+	background(0);
+
+	//draws portal
+/*
+		for(let i = 0;i<portals.length;i++){
+	portals[i].draw();
+
+
+	}
+*/
+
+
+
+	for(let i = 0;i<brickWall.length;i++){
+		brickWall[i].draw();
+
+	}
+
+		if(portals.collide(player)){
+		portals.drawText();
+	}
+
+	portals.draw();
+
+	player.draw();
+
+/* draws portals */ 
+
+
+
+
+}
