698ae2a8504124e6c8ea8a1a494b94554bcab3a5
CalvinGarcia <60408978+CalvinGarcia@users.noreply.github.com>
2020-11-20 15:03:56 -0500

    rock added

--- project3/scene.js
+++ project3/scene.js
@@ -1,7 +1,7 @@
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-camera.position.set( 0, 10,8 );
+camera.position.set( 0, 0.5,10);
 
 const renderer = new THREE.WebGLRenderer( {alpha: true} );
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -36,10 +36,13 @@ const width = 10;
 
 const loader = new THREE.ObjectLoader();
 loader.load( 'scene.json', onLoad );
-// loader.load( ' rock.json' , onLoad);
+loader.load( 'cactus22.json', onLoad);
+loader.load( 'cactus3.json', onLoad);
+loader.load( 'rock.json', onLoad );
 
 
-var p = Math.random () *5 ;
+
+const p = Math.random () *5 ;
 const np = Math.random() *-5;
 
 
@@ -50,17 +53,19 @@ function onLoad( cactus ){
 
 
 	cactus.scale.set( 0.5, 0.5, 0.5);
-	cactus.rotation.y = Math.PI * p;
-	// cactus.position.set( p , 0 , p);
-	cactus.position.x = x;
-	cactus.position.z = p;
-	scene.add( cactus );
+	cactus.rotation.y = Math.PI * 5;
+	cactus.position.set( 8 , 0 , 5);
+	// cactus.position.x = 6;
+	// cactus.position.z = -6;
+	// scene.add( cactus );
 
 
 	
 
 	
 }
+
+
 
 	// cactus.scale.set( 0.5 , 0.5, 0.5);
 	// cactus.rotation.y = Math.PI * p;
@@ -71,6 +76,15 @@ function onLoad( cactus ){
 	animate();
 }
 
+function onLoad( rock ){
+
+	rock.scale.set ( 0.7, 0.7, 0.7 );
+	rock.rotation.y = Math.PI * np;
+	rock.position.set( -5, 0, 5)
+	// rock.position.x = 8;
+	scene.add( rock );
+
+}
 
 
 
